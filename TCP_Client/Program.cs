88aa2e62ff16f82f65bf98daa17e8c4518129ec3
Fyn <thereduzer897@gmail.com>
2024-06-14 15:59:58 +0200

    This can send String messages via tcp

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPCLient
+{
+    public class TCPClient
+    {
+        public static void Main()
+        {
+
+            String ip = "127.0.0.1";
+            int port = 5000;
+            
+            TestClient client = new TestClient(ip, port);
+        }
+    }
+
+    public class TestClient
+    {
+        private TcpClient _client;
+        private StreamReader _sReader;
+        private StreamWriter _sWriter;
+
+        private bool _isConnected;
+
+        public TestClient(String ipaddress, int portnumber)
+        {
+            _client = new TcpClient();
+            _client.Connect(ipaddress, portnumber);
+            
+            HandleCommunication();
+        }
+
+        public void HandleCommunication()
+        {
+            _sReader = new StreamReader(_client.GetStream(), Encoding.ASCII);
+            _sWriter = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+
+            _isConnected = true;
+            String sData = null;
+            while (_isConnected)
+            {
+                Console.Write("Eingabe ");
+                sData = Console.ReadLine();
+
+                _sWriter.WriteLine(sData);
+                _sWriter.Flush();
+            }
+        }
+    }
+}
---
+++ TCP_Testing/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPTesting
+{
+    public class TCPTesting
+    {
+        private static TcpListener server;
+        private static bool isRunning = false;
+        private static int port = 5000;
+        
+        public static void Main()
+        {
+            start();
+        }
+
+        private static void start()
+        {
+            Thread t = new Thread(HandleConnection);
+            t.Start();
+        }
+
+        public static void HandleConnection()
+        {
+            server = new TcpListener(port);
+            server.Start();
+
+
+            while (!isRunning)
+            {
+                TcpClient newClient = server.AcceptTcpClient();
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(newClient);
+            }
+        }
+
+        public static void HandleClient(object obj)
+        {
+            String name;
+
+            TcpClient client = (TcpClient) obj;
+            StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.ASCII);
+
+            bool clientIsConnected = true;
+            string data = String.Empty;
+
+            name = reader.ReadLine();
+
+            while (clientIsConnected)
+            {
+                data = reader.ReadLine();
+                if (data != null)
+                {
+                    Console.WriteLine("Client " + name + " " + data);
+                }
+            }
+        }
+        
+        
+    }
+}
